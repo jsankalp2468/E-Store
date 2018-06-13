61661b869dcb0983b8ba0aec3350cd478cfc9841
shrishti <shrishtiaggarwal29@gmail.com>
2018-06-13 10:29:53 +0530

    user, product, order created

---
+++ src/main/java/com/idfc/estore/entity/Order.java
@@ -0,0 +1,6 @@
+package com.idfc.estore.entity;
+
+public class Order {
+
+    private int orderId;
+}
---
+++ src/main/java/com/idfc/estore/entity/Product.java
@@ -0,0 +1,72 @@
+package com.idfc.estore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int productID;
+    private String productName;
+    private String description;
+    private String price;
+    private String productVariant;
+    private String createdBy;
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getProductVariant() {
+        return productVariant;
+    }
+
+    public void setProductVariant(String productVariant) {
+        this.productVariant = productVariant;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", productName='" + productName + '\'' + ", description='" + description + '\'' + ", price='" + price + '\'' + ", productVariant='" + productVariant + '\'' + ", createdBy='" + createdBy + '\'' + '}';
+    }
+}
